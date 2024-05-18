422671311b4f9db6feb500d43abd4461b57ae2a0
diy-pwa <122644285+diy-pwa@users.noreply.github.com>
2024-05-18 15:08:46 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,15 +3,10 @@
 // Recommended way to include parent theme styles.
 //  (Please see http://codex.wordpress.org/Child_Themes#How_to_Create_a_Child_Theme)
 //  
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array('parent-style')
-    );
+add_action( 'wp_enqueue_scripts', function() {
+    wp_enqueue_style( 'wp-framework7-style', get_stylesheet_directory_uri() . '/style.css');
     wp_enqueue_script('custom_javascript', get_stylesheet_directory_uri() . '/customElements.js');
-}
+});
 //
 // Your code goes below
 //
